068d1aa1dad2aad5b72474c69c69980e22162833
chief <nico.glasser@icloud.com>
2019-02-12 04:40:45 -0800

    Added ava tests.

--- dist/test/test.js
+++ dist/test/test.js
@@ -4,6 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const ava_1 = __importDefault(require("ava"));
-ava_1.default("null", async function (test) {
+const src_1 = __importDefault(require("../src"));
+const level_1 = __importDefault(require("../src/level"));
+ava_1.default("create format", async function (test) {
+    src_1.default.format(level_1.default.ALL, (level, message) => `${level}: ${message}`);
+    test.pass();
+});
+ava_1.default("create logger", async function (test) {
+    src_1.default.save(level_1.default.ALL, "./example.log", false);
+    test.pass();
+});
+ava_1.default("log", async function (test) {
+    src_1.default.log(level_1.default.ALL, "test");
     test.pass();
 });
--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,18 @@
 import ava from "ava";
+import fir from "../src";
+import level from "../src/level";
 
-ava("null", async function(test) {
+ava("create format", async function(test) {
+  fir.format(level.ALL, (level, message) => `${level}: ${message}`);
+  test.pass();
+});
+
+ava("create logger", async function(test) {
+  fir.save(level.ALL, "./example.log", false);
+  test.pass();
+});
+
+ava("log", async function(test) {
+  fir.log(level.ALL, "test");
   test.pass();
 });
