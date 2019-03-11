d142ea1812eec9a57e863f319a104ecf007d2761
Chief <nico.glasser@icloud.com>
2019-03-11 03:45:02 -0700

    Improved testing again

--- test/index.ts
+++ test/index.ts
@@ -1,17 +1,16 @@
 import ava from "ava";
+import fs from "fs";
 import fir from "../src";
 
-ava("create format", async function(test) {
-  fir.format(message => message);
-  test.pass();
-});
-
-ava("create logger", async function(test) {
-  fir.save("./example.log");
-  test.pass();
+fir.setOptions({
+  appendToFile: "latest.log",
+  formatter: function(message) {
+    return `[Test] ${message}`;
+  }
 });
 
 ava("log", async function(test) {
   fir.log("test");
+  fs.unlinkSync(fir.getOptions().appendToFile);
   test.pass();
 });
